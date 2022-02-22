1a53f7939706e32a0f67f749ba42851f13dbc2e8
dymafr <erwan1309@hotmail.com>
2022-02-22 16:32:09 +0100

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <h1>Bonjour le monde !</h1>
+  <input
+    class="input"
+    :class="{ inputOngoing: inputOngoing }"
+    type="text"
+    v-model="input"
+  />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const input = ref('');
+const inputOngoing = computed(() => input.value.length);
+</script>
 
 <style scoped>
-h1 {
-  color: red;
+input {
+  outline: 0;
+  border: 2px solid black;
+  border-radius: 4px;
 }
 </style>
