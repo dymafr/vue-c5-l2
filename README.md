fd39124cce99de07fce3f09239a99ebd99a35bad
dymafr <erwan1309@hotmail.com>
2022-02-22 16:23:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-vnklb4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-vnklb4)
---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Bonjour le monde !</h1>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
